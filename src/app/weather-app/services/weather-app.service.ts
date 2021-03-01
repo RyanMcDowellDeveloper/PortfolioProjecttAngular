b2c525739e34a60809043f75b4e922a890435cfa
Ryan McDowell <ryanmcdowelldeveloper@gmail.com>
2021-02-28 20:30:36 -0500

    082821

--- src/app/weather-app/services/weather-app.service.ts
+++ src/app/weather-app/services/weather-app.service.ts
@@ -11,7 +11,7 @@ export class WeatherAppService {
 
   GetWeatherData(city: string): Observable<any> {
     const weatherServiceQueryString = 'q=' + city + '&appid=ffcaa3563f99d5cd76b5973fca0e38a5&units=Imperial';
-    return this.http.get<any>(encodeURI('http://api.openweathermap.org/data/2.5/weather?' + weatherServiceQueryString));
+    return this.http.get<any>(encodeURI('https://api.openweathermap.org/data/2.5/weather?' + weatherServiceQueryString));
   }
 
 }
